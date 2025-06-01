3843ab691b1c91e7c26ce4c2fb24bad634ca5cdd
Your Name <you@example.com>
2025-05-31 22:49:56 -0400

    implementar lformulario login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,15 +11,4 @@ export const routes: Routes = [
     path: 'auth', // Este será el prefijo para las rutas de autenticación (ej: /auth/login)
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
-
-  // Ejemplo de ruta principal o redirección si es necesario
-  // {
-  //   path: '',
-  //   redirectTo: '/home', // o alguna otra ruta por defecto
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: '**', // Wildcard route para páginas no encontradas
-  //   redirectTo: '/home' // o a una página 404
-  // }
 ];
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule // Opcional
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login implements OnInit {
-  // Aquí puedes definir las propiedades y métodos necesarios para el componente de login
-  constructor() { }
+  loginForm: FormGroup;
+  hidePassword = true; // Para el toggle de visibilidad de contraseña
 
+  private fb = inject(FormBuilder);
+
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
   ngOnInit(): void {
-    // Lógica de inicialización si es necesaria
   }
 
-  // Método para manejar el envío del formulario de login
-  onSubmit(form: any): void {
-    // Lógica para manejar el inicio de sesión
-    console.log('Formulario enviado', form);
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit(): void {
+    
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
   }
 
 }
